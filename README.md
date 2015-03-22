eaa4a271ee6907d93548929d9463d9c917a18e08
Oren Zomer <oren.zomer@gmail.com>
2015-03-22 22:26:33 +0200

    mongodb2 node - first version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-red-node-mongodb2
 MongoDB 2 driver node for Node-RED
+
+Inspired from [node-red-bluemix-nodes](https://github.com/node-red/node-red-bluemix-nodes/tree/master/mongo)
---
+++ mongodb2/mongodb2.js
@@ -0,0 +1,262 @@
+/**
+* Copyright 2015 Awear Solutions Ltd.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+module.exports = function(RED) {
+  "use strict";
+  var url = require("url");
+  var when = require("when");
+  var appEnv = require("cfenv").getAppEnv();
+  var mongodb = require("mongodb");
+
+  var services = [];
+  Object.keys(appEnv.services).forEach(function(label) {
+    if ((/^mongo/i).test(label)) {
+      services = services.concat(appEnv.services[label].map(function(service) {
+        return {
+          "name": service.name,
+          "label": service.label
+        };
+      }));
+    }
+  });
+
+  var operations = {};
+  Object.keys(mongodb.Collection.prototype).forEach(function(operationName) {
+    if ('function' == typeof Object.getOwnPropertyDescriptor(mongodb.Collection.prototype, operationName).value) {
+      operations[operationName] = mongodb.Collection.prototype[operationName];
+    }
+  });
+  // We don't want to pass the find-operation's cursor directly.
+  delete operations.find;
+
+  operations['find.toArray'] = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var callback = args.pop();
+    mongodb.Collection.prototype.find.apply(this, args).toArray(callback);
+  };
+  operations['find.forEach'] = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var callback = args.pop();
+    mongodb.Collection.prototype.find.apply(this, args).forEach(function(doc) {
+      callback(null, doc);
+    }, function(err) {
+      if (err) {
+        callback(err);
+      }
+    });
+  };
+  // override listIndexes with a function that accepts a callback
+  operations.listIndexes = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    var callback = args.pop(); // The real callback, if options was not given.
+    try {
+      callback(null, mongodb.Collection.prototype.listIndexes.apply(this, args));
+    } catch(err) {
+      callback(err);
+    }
+  };
+  RED.nodes.registerType("mongodb2", function Mongo2ConfigNode(n) {
+    RED.nodes.createNode(this, n);
+    this.hostname = n.hostname;
+    this.port = n.port;
+    this.db = n.db;
+    this.name = n.name;
+    var credentials = RED.nodes.getCredentials(n.id);
+    if (credentials) {
+      this.username = credentials.user;
+      this.password = credentials.password;
+    }
+    this.url = url.format({
+      "protocol": "mongodb",
+      "slashes": true,
+      "auth": (this.username?(this.username + ':' + this.password):""),
+      "hostname": this.hostname,
+      "port": this.port,
+      "pathname": this.db
+    });
+  });
+
+  RED.httpAdmin.get('/mongodb2/vcap', function(req, res) {
+    res.json(services);
+  });
+
+  RED.httpAdmin.get('/mongodb2/operations', function(req, res) {
+    res.json(Object.keys(operations).sort());
+  });
+
+  RED.httpAdmin.get('/mongodb2/:id',function(req, res) {
+    var credentials = RED.nodes.getCredentials(req.params.id);
+    if (credentials) {
+      res.json({
+        user: credentials.user,
+        hasPassword: !!credentials.password
+      });
+    } else {
+      res.json({});
+    }
+  });
+
+  RED.httpAdmin.delete('/mongodb2/:id', function(req, res) {
+    RED.nodes.deleteCredentials(req.params.id);
+    res.send(200);
+  });
+
+  RED.httpAdmin.post('/mongodb2/:id', function(req, res) {
+    var newCreds = req.body;
+    var credentials = RED.nodes.getCredentials(req.params.id) || {};
+    if (!newCreds.user) {
+      delete credentials.user;
+    } else {
+      credentials.user = newCreds.user;
+    }
+    if (newCreds.password === "") {
+      delete credentials.password;
+    } else {
+      credentials.password = newCreds.password || credentials.password;
+    }
+    RED.nodes.addCredentials(req.params.id, credentials);
+    res.send(200);
+  });
+
+  var mongoPool = {};
+
+  function getDb(mongoUrl) {
+    var poolCell = mongoPool[mongoUrl];
+    if (!poolCell) {
+      mongoPool[mongoUrl] = poolCell = {
+        instances: 0,
+        promise: when.promise(function(resolve, reject) {
+          mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(db);
+            }
+          });
+        })
+      };
+    }
+    poolCell.instances++;
+    return poolCell.promise;
+  }
+
+  function closeDb(mongoUrl) {
+    var poolCell = mongoPool[mongoUrl];
+    if (!poolCell) {
+      return;
+    }
+    poolCell.instances--;
+    if (poolCell.instances === 0) {
+      delete mongoPool[mongoUrl];
+      poolCell.promise.done(function(db) {
+        db.close();
+      }, function() { // ignore error
+        // db-client was not created in the first place.
+      });
+    }
+  }
+
+  RED.nodes.registerType("mongodb2 in", function Mongo2InputNode(n) {
+    RED.nodes.createNode(this, n);
+    this.configNode = n.configNode;
+    this.collection = n.collection;
+    this.operation = n.operation;
+    if (n.service == "_ext_") {
+      var mongoConfigNode = RED.nodes.getNode(this.configNode);
+      if (mongoConfigNode) {
+        this.url = mongoConfigNode.url;
+      }
+    } else if (n.service) {
+      var configService = appEnv.getService(n.service);
+      if (configService) {
+        this.url = configService.credentials.url || configService.credentials.uri;
+      }
+    }
+    if (!this.url) {
+      this.error("missing mongodb2 configuration");
+      return;
+    }
+    var node = this;
+    getDb(this.url).done(function(db) {
+      var nodeCollection;
+      if (node.collection) {
+        nodeCollection = db.collection(node.collection);
+      }
+      var nodeOperation;
+      if (node.operation) {
+        nodeOperation = operations[node.operation];
+      }
+      node.on("input", function(msg) {
+        var collection = nodeCollection;
+        if (!collection && msg.collection) {
+          collection = db.collection(msg.collection);
+        }
+        if (!collection) {
+          node.error("No collection defined");
+          return;
+        }
+        var operation = nodeOperation;
+        if (!operation && msg.operation) {
+          operation = operations[msg.operation];
+        }
+        if (!operation) {
+          node.error("No operation defined");
+          return;
+        }
+
+        delete msg._topic;
+        delete msg.collection;
+        delete msg.operation;
+
+        var args = msg.payload;
+        if (!Array.isArray(args)) {
+          args = [args];
+        }
+        if (args.length === 0) {
+          // All operations can accept one argument (some can accept more).
+          // Some operations don't expect a single callback argument.
+          args.push(undefined);
+        }
+        if ((operation.length > 0) && (args.length > operation.length - 1)) {
+          // The operation was defined with arguments, thus it may not
+          // assume that the last argument is the callback.
+          // We must not pass too many arguments to the operation.
+          args = args.slice(0, operation.length - 1);
+        }
+        try {
+          operation.apply(collection, args.concat(function(err, result) {
+            if (err) {
+              node.error(err);
+              return;
+            }
+            msg.payload = result;
+            node.send(msg);
+          }));
+        } catch(err) {
+          node.error(err);
+        }
+      });
+    }, function(err) {
+      // Failed to create db client
+      node.error(err);
+    });
+    this.on("close", function() {
+      if (this.url) {
+        closeDb(this.url);
+      }
+    });
+  });
+};
