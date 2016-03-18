fbef73a89865bc4867ab34dd817473d91422f905
Oren Zomer <oren.zomer@gmail.com>
2016-03-18 02:12:03 +0200

    not sure where the connection object is returned - checking two options

--- mongodb2/mongodb2.js
+++ mongodb2/mongodb2.js
@@ -277,10 +277,7 @@ module.exports = function(RED) {
             if (forEachEnd != err) {
               // send msg (when err == forEachEnd, this is just a forEach completion).
               msg.payload = result;
-              if (msg.payload &&
-                msg.payload.result &&
-                msg.payload.result.connection &&
-                (msg.payload.result.connection instanceof EventEmitter)) {
+              if (msg.payload) {
                 // Some operations return a Connection object with the result.
                 // Passing this large connection object might be heavy - it will
                 // be cloned over and over by Node-RED, and there is no reason
@@ -289,7 +286,12 @@ module.exports = function(RED) {
                 // Instead of loading the Connection prototype-function from the
                 // internal libs (which might change their path), I use the fact
                 // that it inherits EventEmitter.
-                delete msg.payload.result.connection;
+                if (msg.payload.connection instanceof EventEmitter) {
+                  delete msg.payload.connection;
+                }
+                if (msg.payload.result && msg.payload.result.connection instanceof EventEmitter) {
+                  delete msg.payload.result.connection;
+                }
               }
               node.send(msg);
             }
